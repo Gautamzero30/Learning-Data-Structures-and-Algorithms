9a74c7c5d9ea339739cc580026e8e3c0dd0a2d80
Gautamzero30 <gautamcse357@gmail.com>
2025-08-03 22:51:23 +0545

    sunday's

--- a.py
+++ a.py
@@ -1,20 +1,21 @@
 import math
 from queue import PriorityQueue
-def INITIALIZE_SINGLE_SOURCE(G,s):
-    cost = dict()
-    prev = dict()
-    for vertex in G.keys():
-        cost[vertex] = math.inf
-        prev[vertex] = " "
-    cost[s]= 0
-    return cost,prev
+# def INITIALIZE_SINGLE_SOURCE(G,s):
+#     cost = dict()
+#     prev = dict()
+#     for vertex in G.keys():
+#         cost[vertex] = math.inf
+#         prev[vertex] = " "
+#     cost[s]= 0
+#     print("aa")
+#     return cost,prev
 
 # from queue import PriorityQueue
 # PQ = PriorityQueue
 # PQ.put((2,"A"))
 # PQ.put((1,"B"))
 # PQ.PUT((3,"C"))
-# while (not PQ.empty()):
+
 
 
 
@@ -34,6 +35,7 @@ def INITIALIZE_SINGLE_SOURCE(G,s):
         cost[vertex] = math.inf
         prev[vertex] = " "
     cost[s]= 0
+    print("cc")
     return cost,prev
 
 
@@ -41,6 +43,7 @@ def RELAX(G,u,v,cost,prev):
     if cost[v] >cost[u] +G[u][v]:
         cost[v]= cost[u]+G[u][v]
         prev[v] = u
+        
     return cost,prev    
 
 
@@ -52,18 +55,27 @@ def DJ(G,s):
     # PQ.PUT((3,"C"))
     for vertex in G.keys():
         PQ.put((cost[vertex], vertex))
+        print(list(PQ.queue))
     visited = []
     while (len(visited) != len(G.keys())):
         _, currentVertex = PQ.get()
+        
         visited.append(currentVertex)
         for chimeki in G[currentVertex].keys():
             if chimeki not in visited:
+                print(visited)
+                print(list(PQ.queue))
                 cost, prev = RELAX(G, currentVertex, chimeki, cost, prev)
-            # PQ.put((cost[chimeki],chimeki))
+                
+                
+                PQ.put((cost[chimeki],chimeki))
+                print(list(PQ.queue))
+                print(prev)
+                print(cost)
     return cost, prev    
 
 # s = "s"
-# cost,prev = DJ(G,s)
+# cost,prev = DJ(G,s) 
 # print(cost)
 # print(prev)
 
@@ -73,6 +85,7 @@ def DJ(G,s):
 def RECONSTRUCT_PATH(vertex, prev):
     path = vertex
     while prev[vertex ] != " ":
+        print(path)
         path  = prev[vertex] + "->" + path
         vertex = prev[vertex]
 
