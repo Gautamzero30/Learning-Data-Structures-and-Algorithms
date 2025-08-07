745291b50c4f47e9fe83d96f0fc0fd1ebc47bc5c
Gautamzero30 <gautamcse357@gmail.com>
2025-08-07 17:45:17 +0545

    today's one

--- Sortings/quicksort .py
+++ Sortings/quicksort .py
@@ -1,36 +1,32 @@
+def quickshort(a, l, r):
+    if l < r:
+        p = partition(a, l, r)
+        quickshort(a, l, p - 1)
+        quickshort(a, p + 1, r)
 
-def quickshort(a,l,r):
-    if (l<r):
-        p = partition(a,l,r)
-        quickshort(a,l,p-1)
-        quickshort(a,p+1,r)
-
-def partition(a,l,r):
-    x=l 
-    y=r       
+def partition(a, l, r):
+    x = l + 1
+    y = r
     pivot = a[l]
-    while(x<y):
-        while (a[x]<= pivot and x <= r):
-            x+=1
-        while( a[y] > pivot):
-            y-=1
-        if (x<y):
-            a[x],a[y]=a[y],a[x]
-
+    while x <= y:
+        while x <= r and a[x] <= pivot:
+            x += 1
+        while a[y] > pivot:
+            y -= 1
+        if x < y:
+            a[x], a[y] = a[y], a[x]
+            print("Swapped:", a)
         else:
             break
-    a[l],a[y]=a[y],a[l]    
+    a[l], a[y] = a[y], a[l]
     return y
 
-a= []
-n = int(input("enter the no. of elements you want to sort"))
+# Input
+a = []
+n = int(input("Enter the number of elements you want to sort: "))
 for i in range(n):
-    p=int(input(f"enter the {i+1 } element") )
+    p = int(input(f"Enter the {i + 1} element: "))
     a.append(p)
 
-quickshort(a,0,n-1)
-print("the sorted list is", a)
-
-
-
-          
+quickshort(a, 0, n - 1)
+print("The sorted list is", a)
