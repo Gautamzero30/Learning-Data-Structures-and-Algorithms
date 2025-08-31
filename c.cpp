1278c9a7ad08155542362b6ee5dcf6618563b095
Gautamzero30 <gautamcse357@gmail.com>
2025-08-31 22:22:23 +0545

    today's one

---
+++ c.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited) {
+    visited[node] = true;
+    cout << node << " ";
+    for (int neighbor : adj[node]) {
+        if (!visited[neighbor]) dfs(neighbor, adj, visited);
+    }
+}
+
+int main() {
+    int n = 5; // number of nodes
+    vector<vector<int>> adj(n);
+    adj[0] = {1, 2};
+    adj[1] = {0, 3};
+    adj[2] = {0, 4};
+    adj[3] = {1};
+    adj[4] = {2};
+
+    vector<bool> visited(n, false);
+    cout << "DFS Traversal: ";
+    dfs(0, adj, visited);
+}
